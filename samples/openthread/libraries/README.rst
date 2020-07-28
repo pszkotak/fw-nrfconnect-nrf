ec7ba87076cc9376ea6f12093127c48775a0659c
Piotr Szkotak <piotr.szkotak@nordicsemi.no>
2020-07-28 10:51:51 +0200

    Build OT libraries

---
+++ samples/openthread/libraries/README.rst
@@ -0,0 +1,6 @@
+.. _ot_lib_sample:
+
+Thread: OpenThread Librares
+###########
+
+This sample is used to build and copy the generated libs into the approprate nrfxlib directory.
---
+++ samples/openthread/libraries/src/main.c
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+void main(void)
+{
+	// Intentionally empty.
+}
